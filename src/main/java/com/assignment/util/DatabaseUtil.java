d106a6fac64d8a0aba641ba07eb4bc55cf275fae
nicoleajukwu <nicoleajukwu@gmail.com>
2025-10-06 00:04:58 -0500

    Update DatabaseUtil.java

--- src/main/java/com/assignment/util/DatabaseUtil.java
+++ src/main/java/com/assignment/util/DatabaseUtil.java
@@ -3,14 +3,12 @@ package com.assignment.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class DatabaseUtil {
-    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    private static final String URL = dotenv.get("DB_URL", "jdbc:mysql://localhost:3306/zumba_class");
-    private static final String USERNAME = dotenv.get("DB_USERNAME", "root");
-    private static final String PASSWORD = dotenv.get("DB_PASSWORD", "password");
-    
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String USERNAME = System.getenv("DB_USERNAME");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -18,18 +16,8 @@ public class DatabaseUtil {
             throw new RuntimeException("MySQL JDBC Driver not found", e);
         }
     }
-    
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-    }
-    
-    public static void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
     }
 }
